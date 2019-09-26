09f2ea582d3e0658f99a667e19dd53a71ed12989
dominait <45467640+dominait@users.noreply.github.com>
2019-09-26 11:35:28 -0600

    dfjlsadjfldsj

---
+++ cpsc-1520-ex-06-functions/js/main.js
@@ -0,0 +1,23 @@
+/* 
+    Functions In Javascript
+    - when you start writing the same code again and again and agina and again
+*/
+
+// Arrow Functions
+function makeFries(veg, oil){
+    return "fries"
+}
+// Explicit Arrow Function
+const makeFries = (veg, oil) =>{
+    return "fries"
+}
+
+// Implicit Arrow function
+const makeFries = (veg, oil)=> "fries"
+
+makeFries("orange", "car")
+ 
+
+ 
+
+

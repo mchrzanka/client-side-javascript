12a3ceffcc4a40871421da8b9f9e75b9f70ccaed
dominait <45467640+dominait@users.noreply.github.com>
2019-09-30 09:51:21 -0600

    Updated Example Sevent

--- cpcs1520-ex-07-sweed/js/main.js
+++ cpcs1520-ex-07-sweed/js/main.js
@@ -1,1 +1,34 @@
-console.log("hi I am the javascript file")
+/* 
+   get ref var to
+     form, product select, output
+*/
+
+const options = document.querySelector('#options')
+const products = document.querySelector('#products')
+const productImage = document.querySelector('#output img')
+
+// submitting a form handler
+options.addEventListener('submit', function(e){
+       // stop the default behavhour
+       e.preventDefault();     
+})
+
+// change handler for the product
+// switch the image from the value property???
+// event objct see what properties it returns
+
+ products.addEventListener('change', (e) => {
+      const imageUrl = `img/${event.target.value}.jpg`;
+      productImage.src = imageUrl;
+      // options returns an array
+      const temp = Array.from (e.target.options)
+      const index = e.target.selectedIndex
+     console.log(temp[index])
+      
+      formatProductName()
+ })
+
+
+ function formatProductName(){
+
+ }

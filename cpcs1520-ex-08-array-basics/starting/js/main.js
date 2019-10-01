c4aa08c656105fc6b0b012e193a8cac2ab0e36bd
jimbits <contactjamesfoley@gmail.com>
2019-09-30 20:57:08 -0600

    updated exercise 8

---
+++ cpcs1520-ex-08-array-basics/starting/js/main.js
@@ -0,0 +1,13 @@
+/* 
+      get ref var to
+     form, product select, output
+*/
+const productPrices = [13.33, 8.65, 10.23, 9.65, 10.69];
+const shippingRates = [16.55, 24.33, 21.12, 19.78, 26.18];
+const productTypes = ["Thor", "Purple Urkle", "MK Ultra", "Gorilla Glue"];
+
+const options = document.querySelector("#options");
+const products = document.querySelector("#products");
+const display = document.querySelector("#output");
+
+ 

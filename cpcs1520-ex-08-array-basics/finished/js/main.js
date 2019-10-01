ca67f8e608fe63f39ff99d5adb7baf8a78cd0c52
dominait <45467640+dominait@users.noreply.github.com>
2019-10-01 14:52:51 -0600

    updated starting files for example 8

--- cpcs1520-ex-08-array-basics/finished/js/main.js
+++ cpcs1520-ex-08-array-basics/finished/js/main.js
@@ -4,25 +4,35 @@
 */
 const productPrices = [13.33, 8.65, 10.23, 9.65, 10.69];
 const shippingRates = [16.55, 24.33, 21.12, 19.78, 26.18];
-const productTypes = ["Thor", "Purple Urkle", "MK Ultra", "Gorilla Glue"];
 
 const options = document.querySelector("#options");
 const products = document.querySelector("#products");
 const display = document.querySelector("#output");
 
-// submitting a form handler
-options.addEventListener("submit", function(e) {
-  // stop the default behavhour
-  e.preventDefault();
-});
+const productTypes = ["Thor", "Purple Urkle", "MK Ultra", "Gorilla Glue"];
+// select event handler for the products
+products.addEventListener('change', function(e){
+    // getting an option field in a select element
+    const url =  `img/${e.target.value}.jpg`    
+    output.querySelector('img').src =url;
+   const productItem =   productTypes[e.target.selectedIndex]
+   output.querySelector('p').textContent = productItem
+   
+})
 
-// change handler for the product
-// switch the image from the value property???
-// event objct see what properties it returns
+// submit handler for the form
+options.addEventListener('submit', function(e){
+     e.preventDefault();
+     const quantity= e.target.quantity.value;
+     const selectedProduct = productTypes[e.target.products.selectedIndex]
+     const invoice = `
+         <ul>
+            <li>Product Type: ${selectedProduct} </li>
+            <li>Quantity: ${quantity}</li>
+         </ul>
+     `
+      display.querySelector('div').innerHTML = invoice;
+})
 
-products.addEventListener("change", e => {
-  const imageUrl = `img/${event.target.value}.jpg`;
-  display.querySelector("img").src = imageUrl;
-  display.querySelector("p").textContent = productTypes[e.target.selectedIndex];
-  console.log(e.target.selectedIndex);
-});
+
+ 
